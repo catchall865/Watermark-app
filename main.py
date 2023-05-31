77b13e66df3b5e4002ebac74f10507f0cb7ed668
Mark <markunder2022@protonmail.com>
2023-05-31 15:10:45 -0400

    create exe

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from tkinter import *
-from tkinter import ttk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from PIL import Image, ImageTk, ImageDraw, ImageFont
-from io import BytesIO
-import requests
 
 class App(Tk):
     def __init__(self, *args, **kwargs):
